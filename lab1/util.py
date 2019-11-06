d53d79e5201bd1d72cbd86dc78b61dc989d09727
MarshallCharles <cjm.pcan2@gmail.com>
2019-11-06 13:12:00 +0100

    Adding new code

---
+++ lab1/util.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn import mixture
+
+def df_fromarr(names, *args):
+    df = pd.DataFrame()
+    for i in range(len(args)):
+        df[names[i]] = args[i]
+    return df
+
+def plot_letter_data(letter):
+    xs = []
+    ys = []
+    files = [f for f in os.listdir("./Unistroke/") if ((f[0] == letter) and (f[1] != 'm'))]
+    for item in files:
+        with open("./Unistroke/"+item,'r') as f:
+            for line in f.readlines():
+                data = line.split('\t')
+                for i in range(len(data)):
+                    data[i] = float(data[i])
+                xs.append(data[0])
+                ys.append(data[1])
+    return df_fromarr(['x','y'],xs,ys)
+
+def get_letter_data(letter):
+    data = np.empty((1,2))
+    files = [f for f in os.listdir("./Unistroke/") if ((f[0] == letter) and (f[1] != 'm'))]
+    for item in files:
+        data = np.vstack((data, np.loadtxt("./Unistroke/"+item)))
+    return data
+
+def fitpredict_gmm(data):
+    model = mixture.GaussianMixture(n_components = 2)
+    labs = model.fit_predict(data)
+    return labs
+
+def prep_work_GMM():
+    mean1 = [-3,0]
+    mean2 = [3,0]
+    lab1 = [1]*150
+
+    cov1 = [[5,-2],[-2,1]]
+    cov2 = [[5,2],[2,2]]
+    lab2 = [2]*350
+
+    x,y = np.random.multivariate_normal(mean1, cov1, size = 150).T
+    x2, y2 = np.random.multivariate_normal(mean2, cov2, 350).T
+    xs = list(x) + list(x2)
+    ys = list(y) + list(y2)
+    labs = lab1 + lab2
+    return df_fromarr(['x','y','class'], xs,ys,labs)
--- lab1/work.py
+++ lab1/work.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import altair as alt
-from os import listdir
+import util
+from sklearn import mixture
 
 def plot_from_df(df):
     if len(list(df))>2:
@@ -15,62 +16,20 @@ def plot_from_df(df):
                 alt.Y('y:Q')).interactive()
     chart.serve()
 
-def df_fromarr(names, *args):
-    df = pd.DataFrame()
-    for i in range(len(args)):
-        df[names[i]] = args[i]
-    return df
-
-def get_A_data():
+def main():
+    #plot_from_df(util.prep_work_GMM())
+    #plot_from_df(util.plot_letter_data('A'))
+    data = util.get_letter_data('A')
+    #for item in data:
+    #    print(item)
+    labs = util.fitpredict_gmm(data)
     xs = []
     ys = []
-    '''
-    with open("./Unistroke/Amerge.txt", 'r') as f:
-        for line in f.readlines():
-            data = line.split(' ')
-            for i in range(len(data)):
-                data[i] = float(data[i])
-            xs.append(data[0])
-            ys.append(data[1])
-
-    ''' 
-    files = [f for f in listdir("./Unistroke/") if ((f[0] == 'A') and (f[1] != 'm'))]
-    for item in files:
-        print(item)
-        with open("./Unistroke/"+item,'r') as f:
-            for line in f.readlines():
-                data = line.split('\t')
-                for i in range(len(data)):
-                    data[i] = float(data[i])
-                xs.append(data[0])
-                ys.append(data[1])
-    df = df_fromarr(['x','y'],xs,ys)
-    print(df)
-    plot_from_df(df)
-
-def prep_GMM():
-    mean1 = [-3,0]
-    mean2 = [3,0]
-    lab1 = [1]*150
-
-    cov1 = [[5,-2],[-2,1]]
-    cov2 = [[5,2],[2,2]]
-    lab2 = [2]*350
-    
-    x,y = np.random.multivariate_normal(mean1, cov1, size = 150).T
-    x2, y2 = np.random.multivariate_normal(mean2, cov2, 350).T 
-    
-    xs = list(x) + list(x2)
-    ys = list(y) + list(y2)
-    labs = lab1 + lab2
-    
-    df = df_fromarr(['x','y','class'], xs,ys,labs)
-    
-    plot_from_df(df)
-
-def main():
-    #prep_GMM()
-    get_A_data()
+    for item in data:
+        xs.append(float(item[0]))
+        ys.append(float(item[1]))
+    df = util.df_fromarr(['x','y','label'],xs,ys,list(labs))
+    plot_from_df(util.df_fromarr(['x','y','class'],xs,ys,list(labs)))
 
 if __name__=='__main__':
     main()
