4602b0b035858069c0b2b62eacf5c336d50476af
MarshallCharles <cjm.pcan2@gmail.com>
2019-11-22 15:44:31 +0100

    new work

--- lab1/util.py
+++ lab1/util.py
@@ -1,18 +1,37 @@
 import numpy as np
 import pandas as pd
+import statistics as stat
 import os
 from sklearn import mixture
+from matplotlib.colors import LogNorm
 from matplotlib import pyplot as plt
+import altair as alt
 
-def df_fromarr(names, *args):
+def plot(*args):
+    charts = []
+    for item in args:
+        if type(item) is str:
+            charts[-1].title = item
+        elif len(list(item)) > 2:
+            charts.append(alt.Chart(item, height = 250, width = 250).mark_point(filled = True).encode(
+                    alt.X("x:Q"), alt.Y("y:Q"), alt.Color("label:N")).interactive())
+        else:
+            charts.append(alt.Chart(item,height = 250, width = 250).mark_point(filled = True).encode(
+                    alt.X('x:Q'),alt.Y('y:Q')).interactive())
+    for i in range(len(charts)):
+        if i==0: chart = charts[i]
+        else: chart = chart | charts[i]
+    chart.display()
+
+def df_fromarr(*args, **kwargs):
     df = pd.DataFrame()
-    for i in range(len(args)):
-        df[names[i]] = args[i]
+    for key, val in kwargs.items():
+        df[key] = val
     return df
 
-def get_data(pathto, files, plot_ready = False):
+def get_data(pathto, *args):
     flag = 0
-    for item in files:
+    for item in args:
         if flag == 0:
             data = np.loadtxt(pathto+item)
             flag = 1
@@ -20,29 +39,34 @@ def get_data(pathto, files, plot_ready = False):
             data = np.vstack((data, np.loadtxt(pathto+item)))
     return data
 
-def prep_work_GMM():
-    lab1 = [1]*150
-    lab2 = [2]*350
-    x,y = np.random.multivariate_normal([-3,0], [[5,-2],[-2,1]], size = 150).T
-    x2, y2 = np.random.multivariate_normal([3,0], [[5,2],[2,2]], 350).T
-    xs = list(x) + list(x2)
-    ys = list(y) + list(y2)
-    labs = lab1 + lab2
-    return df_fromarr(['x','y','class'], xs,ys,labs)
+def mean_variance(df, **kwargs):
+    name = kwargs['name'] if 'name' in kwargs else 'Input Model'
+    zeromean = [round(i,2) for i in [stat.mean([x for x in df['x'][df['label']==0]]),\
+                stat.mean([x for x in df['y'][df['label']==0]])]]
+    onemean = [round(i,2) for i in [stat.mean([x for x in df['x'][df['label']==1]]),\
+                stat.mean([x for x in df['y'][df['label']==1]])]]
+    covone = np.cov([x for x in df['x'][df['label']==1]],[x for x in df['y'][df['label']==1]])
+    covzero = np.cov([x for x in df['x'][df['label']==0]],[x for x in df['y'][df['label']==0]])
+    print("Parameters for {}:\n".format(name),"="*25,"\n\
+Mean of class 0 = {}, (co)-variance of class 0 = {}\n\
+Mean of class 1 = {}, (co)-variance of class 1 = {}\n".format(zeromean,np.round(covone,2).tolist(),onemean,np.round(covzero,2).tolist()))
 
-def plot_pdf(model, values, labels, comp):
-    x = np.linspace(-1.5,1.5)
-    y = np.linspace(-1.5,1.5)
-    X, Y = np.meshgrid(x,y)
-    XX = np.array([X.ravel(), Y.ravel()]).T
-    Z = -model.score_samples(XX)
-    Z = Z.reshape(X.shape)
-    # plot contour
-    CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
-                     levels=np.logspace(0, 3, 10))
+def plot_pdf(model, data, labels):
+    maxi= max(x[0] for x in data)
+    mini = min(x[0] for x in data)
+    maxi = maxi + (maxi-mini)/8
+    mini = mini - (maxi-mini)/8
+    x, y = np.meshgrid(np.linspace(mini,maxi),np.linspace(mini,maxi))
+    XX = np.array([x.ravel(), y.ravel()]).T
+    z = -model.score_samples(XX)
+    z = z.reshape(x.shape)
+    CS = plt.contour(x, y, z, norm=LogNorm(vmin=1.0, vmax=1000.0),
+                    levels=np.logspace(0, 3, 10))
     CB = plt.colorbar(CS, shrink=0.8, extend='both')
-
     plt.scatter(data[:, 0], data[:, 1],.8, c=labels)
     plt.title('Negative log-likelihood predicted by a GMM')
     plt.axis('tight')
     plt.show()
+
+def transform_points(data):
+    x = 1
--- lab1/work.py
+++ lab1/work.py
@@ -6,60 +6,34 @@ import altair as alt
 import os
 import util
 
-def scatter_from_df(*args):
-    charts = []
-    for item in args:
-        if len(list(item))>2:
-            charts.append(alt.Chart(item, height = 500, width = 500).mark_point(filled = True).encode(
-                    alt.X("x:Q"), alt.Y("y:Q"), alt.Color("class:N")).interactive())
-        else:
-            charts.append(alt.Chart(item,height = 500, width = 500).mark_point(filled = True).encode(
-                    alt.X('x:Q'),alt.Y('y:Q')).interactive())
-    for i in range(len(charts)):
-        if i==0: chart = charts[i]
-        else: chart = chart | charts[i]
-    chart = chart.configure_legend(clipHeight = 10, titleFontSize= 17, \
-                            labelFontSize = 17, symbolSize = 50, \
-                            symbolStrokeWidth = 3)\
-            .configure_axis(labelFontSize=17, titleFontSize=17)
-    chart.serve()
-
 def main():
-    '''
-    #1. Preliminary Questions
-    scatter_from_df(util.prep_work_GMM())
-    A_data = util.get_data("./Unistroke/",[f for f in os.listdir("./Unistroke/") if ((f[0] == 'A') and (f[1] != 'm'))])
-    scatter_from_df(util.df_fromarr(['x','y'],[x[0] for x in A_data],[x[1] for x in A_data]))
+    A = util.get_data("./Unistroke/",*[f for f in os.listdir("./Unistroke/") if ((f[0] == 'A') and (f[1] != 'm'))])
+    flag = 0
+    for item in list(A):
+        if item[0] < 0:
+            continue
+        elif flag == 0:
+            new = np.array(item)
+            flag = 1
+        else:
+            new = np.vstack((new, item))
+    A = new
 
-    #2. Data Analysis, Gaussian Models
-    scatter_from_df(util.df_fromarr(['x','y'],[x[0] for x in A_data],[x[1] for x in A_data]),\
-                    util.df_fromarr(['x','y','class'],[x[0] for x in A_data],[x[1] for x in A_data],\
-                    list(mixture.GaussianMixture(n_components = 2).fit_predict(A_data))))
-    '''
-    Amerge_data = util.get_data("./Unistroke/", ["Amerge.txt",])
-    '''
-    scatter_from_df(util.df_fromarr(['x','y'],[x[0] for x in Amerge_data],[x[1] for x in Amerge_data]),\
-                    util.df_fromarr(['x','y','class'],[x[0] for x in Amerge_data],[x[1] for x in Amerge_data],\
-                    list(mixture.GaussianMixture(n_components = 2).fit_predict(Amerge_data))))
-    '''
-    Amerge_df =  util.df_fromarr(['x','y','class'],[x[0] for x in Amerge_data],[x[1] for x in Amerge_data],\
-                                    list(mixture.GaussianMixture(n_components = 2).fit_predict(Amerge_data)))
-    '''
-    ones = [[x,y] for x,y,z in zip(Amerge_df['x'],Amerge_df['y'],Amerge_df['class']) if z==1]
-    zeros = [[x,y] for x,y,z in zip(Amerge_df['x'],Amerge_df['y'],Amerge_df['class']) if z!=1]
-    onemean = [sum([x[0] for x in ones])/len(ones), sum([x[1] for x in ones])/len(ones)]
-    zeromean = [sum([x[0] for x in zeros])/len(zeros), sum([x[1] for x in zeros])/len(zeros)]
-    covone = np.cov(np.array(ones).T)
-    covzero = np.cov(np.array(zeros).T)
-    print("Parameters for the bivariate GMM on the letter A data:\n","="*25,"\n\
-Mean of class 0 = {}\n\
-Mean of class 1 = {}\n\
-Covariance of class 0 = {}\n\
-Covariance of class 1 = {}".format(zeromean,onemean,covone,covzero))
-    '''
-    GMM = mixture.GaussianMixture(n_components = 2)
-    GMM.fit_predict(Amerge_data)
-    util.plot_pdf(GMM, 0, 0, 0)
+    A_GMM = mixture.GaussianMixture(n_components = 2)
+    A_labeled_df = util.df_fromarr(x = [x[0] for x in A],\
+                                    y = [x[1] for x in A],label= A_GMM.fit_predict(A))
+    Amerge = util.get_data("./Unistroke/", "Amerge.txt")
+    Amerge_GMM = mixture.GaussianMixture(n_components = 2)
+    Amerge_labeled_df = util.df_fromarr(x = [x[0] for x in Amerge],\
+                                        y = [x[1] for x in Amerge],\
+                                        label= Amerge_GMM.fit_predict(Amerge))
+
+    '''1. Preliminary Questions'''
+    # util.plot((util.prep_work_GMM()))
+    # util.plot((util.df_fromarr(x = [x[0] for x in A],\
+    #                                 y = [x[1] for x in A])))
+    # '''2. Data Analysis, Gaussian Models'''
+    util.mean_variance(A_labeled_df)
 
 
 if __name__=='__main__':
